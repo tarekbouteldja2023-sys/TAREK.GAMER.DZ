da79c677115104a35406e0be1a6d66085eda3eb0
tarekbouteldja2023-sys <tarekbouteldja2023@gmail.com>
2026-01-30 12:57:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// صوت النقر
+// تشغيل الموسيقى بعد أول نقرة
 document.addEventListener("click", () => {
+  const music = document.getElementById("bgMusic");
+  if (music.paused) {
+    music.play().catch(e => console.log("Audio requires user interaction"));
+  }
+
+  // صوت النقر
   const click = document.getElementById("clickSound");
   click.currentTime = 0;
   click.play();
 });
-
-// تشغيل الموسيقى بعد أول تفاعل
-document.addEventListener("click", () => {
-  document.getElementById("bgMusic").play();
-}, { once: true });
